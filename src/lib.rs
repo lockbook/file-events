5d318aad76cfacdab10c7643286ed89e79b4856a
ssapra2 <ssapra2@andrew.cmu.edu>
2023-07-12 17:46:00 -0400

    More Tests, Rename, MoveOut

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
-use std::path::{PathBuf, Path};
-use std::sync::mpsc;
+use std::collections::VecDeque;
+use std::ops::Deref;
+use std::path::{Path, PathBuf};
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
+use std::time::Duration;
+
+use fsevent::{Event, StreamFlags};
 
 #[derive(Debug, PartialEq)]
 pub enum FileEvent {
@@ -11,44 +17,80 @@ pub enum FileEvent {
     MoveOut(PathBuf),
 }
 
+pub struct RenameState {
+    path: PathBuf,
+    id: u64,
+}
+
+#[derive(Clone)]
 pub struct Watcher {
     dest: PathBuf,
+    rename_candidate: Arc<Mutex<Option<RenameState>>>,
 }
 
 impl Watcher {
     pub fn new(dest: PathBuf) -> Self {
         Self {
             dest,
+            rename_candidate: Default::default(),
         }
     }
 
     pub fn watch_for_changes(&self) -> mpsc::Receiver<FileEvent> {
-        //let mut watcher = Watcher::new(".".into());
-
         let (sender, receiver) = mpsc::channel();
         let (tx, rx) = mpsc::channel();
         let deststr = self.dest.as_os_str().to_str().unwrap().to_string();
 
-        let _t = thread::spawn(move || {
+        thread::spawn(move || {
             let fsevent = fsevent::FsEvent::new(vec![deststr]);
             fsevent.observe(sender);
         });
 
-        let _t2 = thread::spawn(move || {
-            loop {
-                println!("Here");
-                let val = receiver.recv();
-                println!("{:?}", val.as_ref().unwrap());
-                let location = &val.as_ref().unwrap().clone().path;
-                if val.as_ref().unwrap().flag.contains(fsevent::StreamFlags::ITEM_CREATED){
-                    tx.send(FileEvent::Create((location.to_string()).into())).unwrap();
-                }
-                else if val.as_ref().unwrap().flag.contains(fsevent::StreamFlags::ITEM_REMOVED){
-                    tx.send(FileEvent::Remove((location.to_string()).into())).unwrap();
-                }
-            }
+        let event_loop_state = self.clone();
+        thread::spawn(move || {
+            event_loop_state.event_loop(receiver, tx);
         });
 
         rx
+    }
+
+    fn event_loop(&self, fsevent_rx: Receiver<Event>, tx: Sender<FileEvent>) {
+        loop {
+            let val = fsevent_rx.recv().unwrap();
+            println!("{:?}", &val);
+            let location = PathBuf::from(&val.path);
+            let flags = &val.flag;
+            if flags.contains(StreamFlags::ITEM_CREATED)
+                && !flags.contains(StreamFlags::ITEM_REMOVED)
+                && !flags.contains(StreamFlags::ITEM_RENAMED)
+            {
+                tx.send(FileEvent::Create(location)).unwrap();
+            } else if flags.contains(StreamFlags::ITEM_REMOVED) {
+                tx.send(FileEvent::Remove(location)).unwrap();
+            } else if flags.contains(StreamFlags::ITEM_RENAMED) {
+                let mut rename_candidate = self.rename_candidate.lock().unwrap();
+                if let Some(pending) = rename_candidate.take() {
+                    if pending.id == val.event_id - 1 {
+                        tx.send(FileEvent::Rename(pending.path, location)).unwrap();
+                    } else {
+                        tx.send(FileEvent::MoveOut(pending.path)).unwrap();
+                        tx.send(FileEvent::MoveOut(location)).unwrap();
+                    }
+                } else {
+                    *rename_candidate = Some(RenameState {
+                        path: location,
+                        id: val.event_id,
+                    });
+                    let clone_self = self.clone();
+                    let clone_tx = tx.clone();
+                    thread::spawn(move || {
+                        thread::sleep(Duration::from_millis(100));
+                        if let Some(pending) = clone_self.rename_candidate.lock().unwrap().take() {
+                            clone_tx.send(FileEvent::MoveOut(pending.path)).unwrap();
+                        }
+                    });
+                }
+            }
+        }
     }
 }
--- tests/scenarios.rs
+++ tests/scenarios.rs
@@ -1,17 +1,54 @@
-use std::{path::PathBuf, fs, time::Duration, thread};
+use std::{fs, path::PathBuf, thread, time::Duration};
 
-use file_events::{Watcher, FileEvent};
+use file_events::{FileEvent, Watcher};
+
+fn create_dir() -> PathBuf {
+    let path = uuid::Uuid::new_v4();
+    let path = PathBuf::from(format!("./{path}"));
+    fs::create_dir(&path).unwrap();
+    thread::sleep(Duration::from_millis(100));
+    path.canonicalize().unwrap()
+}
 
 #[test]
 fn create_test() {
-    let watcher = Watcher::new(PathBuf::from("."));
+    let dest = create_dir();
+    let watcher = Watcher::new(dest.clone());
     let x = watcher.watch_for_changes();
-    let dest = "./new_file";
-    fs::File::create(&dest).unwrap();
-    println!("{:?}", x);
-    //thread::sleep(Duration::from_millis(1500));
+    let mut newfile = dest.clone();
+    newfile.push("new_file");
+    fs::File::create(&newfile).unwrap();
     let evt = x.recv().unwrap();
-    assert_eq!(evt, FileEvent::Create("./new_file".into()));
-    
+    assert_eq!(evt, FileEvent::Create(newfile));
+    fs::remove_dir_all(dest).unwrap();
 }
 
+#[test]
+fn remove_test() {
+    let dest = create_dir();
+    let mut newfile = dest.clone();
+    newfile.push("new_file");
+    fs::File::create(&newfile).unwrap();
+    let watcher = Watcher::new(dest.clone());
+    let x = watcher.watch_for_changes();
+    fs::remove_file(&newfile).unwrap();
+    let evt = x.recv().unwrap();
+    assert_eq!(evt, FileEvent::Remove(newfile));
+    fs::remove_dir_all(dest).unwrap();
+}
+
+#[test]
+fn rename_test() {
+    let dest = create_dir();
+    let mut newfile = dest.clone();
+    newfile.push("new_file");
+    let mut newfile2 = dest.clone();
+    newfile2.push("new_file_2");
+    fs::File::create(&newfile).unwrap();
+    let watcher = Watcher::new(dest.clone());
+    let x = watcher.watch_for_changes();
+    fs::rename(&newfile, &newfile2).unwrap();
+    let evt = x.recv().unwrap();
+    assert_eq!(evt, FileEvent::Rename(newfile, newfile2));
+    fs::remove_dir_all(dest).unwrap();
+}
